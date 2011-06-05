b98b6d5fae9af5b3143226312c4ebfc60273195a
yoda <yoda@tallon4.net>
2011-06-05 22:16:23 +0800

    actually got the toy to work, kind of

--- ParadigmsProject/LockingToy.fs
+++ ParadigmsProject/LockingToy.fs
@@ -29,27 +29,57 @@ let waitFor obj = ignore(Monitor.Wait obj)
 /// wake up all threads waiting on a given object
 let wakeWaiters obj = Monitor.PulseAll obj
 
+type ToyLocker () =
+    let workQueue = Queue()
+    
+    let stop_work = ref false
+    let queuelength_internal = ref workQueue.Count
+    let work_loop () = 
+        while(not stop_work.Value) do
+            lock queuelength_internal <| fun() -> 
+                while(queuelength_internal.Value = 0) do
+                    printfn "Thread: %s is waiting" Thread.CurrentThread.Name
+                    waitFor queuelength_internal
+                Console.Out.WriteLine( (sprintf "Thread: %s woke up because of new data: %d" Thread.CurrentThread.Name, workQueue.Peek))
+                workQueue.Dequeue; queuelength_internal := (queuelength_internal.Value - 1)
+                wakeWaiters queuelength_internal
+        ()
+            
+    member this.enqueue obj = lock queuelength_internal <| fun() -> 
+        workQueue.Enqueue obj
+        queuelength_internal := (queuelength_internal.Value + 1)
+        wakeWaiters queuelength_internal
+    member this.dequeue = workQueue.Dequeue; queuelength_internal := (queuelength_internal.Value - 1)
+    member this.listener = startThread "Thread Listener" work_loop
+    member this.queuelength = queuelength_internal
+
+    
 
 let sharedInt = ref 0
 
-let lockaholic () = 
-    for i in [1..1000] do
-        lock sharedInt <| fun() -> 
-            sleep (random 5)
-            sharedInt := (sharedInt.Value + 1) 
-            printfn "Thread: %s incremented sharedInt: %d" Thread.CurrentThread.Name, sharedInt.Value
-        |> ignore
-    ()
-
-let threada = Thread (ThreadStart lockaholic, Name="Thread A")
-startThread "Thread B" lockaholic
-threada.Start()
 
 
-printfn "Shared Int: %d" sharedInt.Value
+if false then
+    let lockaholic () = 
+        for i in [1..1000] do
+            lock sharedInt <| fun() -> 
+                sleep (random 5)
+                sharedInt := (sharedInt.Value + 1) 
+                printfn "Thread: %s incremented sharedInt: %d" Thread.CurrentThread.Name, sharedInt.Value
+            |> ignore
+        ()
 
-System.Console.ReadLine() |> ignore
+    let threada = Thread (ThreadStart lockaholic, Name="Thread A")
+    startThread "Thread B" lockaholic
+    threada.Start()
 
-printfn "Shared Int: %d" sharedInt.Value
+printfn "Start Listener"
+let locker = new ToyLocker()
 
-System.Console.ReadLine() |> ignore
+locker.listener
+
+while(true) do
+    System.Console.ReadLine() |> ignore
+    printfn "Enqueueing item to listener"
+    locker.enqueue(random 10)
+    printfn "Number of Enqueued items: %d" locker.queuelength.Value
